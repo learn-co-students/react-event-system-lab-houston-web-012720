139fada491f13b4ec0988e05bc0730d096014916
somaia-khalil <sumaya_khair@live.com>
2020-03-30 19:53:15 -0500

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React from "react";
+
+export default class Keypad extends React.Component{
+
+    onkeyup = () => {
+        console.log('Entering password...')
+    }
+
+    render(){
+        return (
+            <div>
+                <input type="password"  onKeyUp={this.onkeyup}></input>
+            </div>
+        )
+
+    }
+
+}
